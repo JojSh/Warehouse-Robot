014abdce84ff4a0c3032ba07d8c9f135f4a4aac3
Toby Clarke <teeohbee@gmail.com>
2016-05-25 22:26:00 +0100

    Robot can move east and west

--- lib/warehouse.rb
+++ lib/warehouse.rb
@@ -13,6 +13,32 @@ class Warehouse
             [0,0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0,0]]
-  end
+    end
+
+
+    def place_robot(x, y)
+      @grid[x][y] = 1
+    end
+
+    def move_robot(direction)
+      x = locate_robot[0]
+      y = locate_robot[1]
+      row = @grid[x]
+      if direction == "W"
+        row.insert(y-1, row.delete_at(y))
+      else
+        row.insert(y+1, row.delete_at(y))
+      end
+    end
+
+    def locate_robot
+      co_ords = []
+      @grid.each_with_index do | row , index |
+        if row.include?(1)
+          co_ords = [index, row.index(1)]
+        end
+      end
+      return co_ords
+    end
 
 end
--- spec/warehouse_spec.rb
+++ spec/warehouse_spec.rb
@@ -2,10 +2,53 @@ require 'warehouse'
 
 describe Warehouse do
 
+  subject(:warehouse) { described_class.new }
+
   it 'is initialized as a 10x10 grid' do
-    warehouse = Warehouse.new
     expect(warehouse.grid.length).to eq(10)
-    expect(warehouse.grid[0].length).to eq(10)    
+    expect(warehouse.grid[0].length).to eq(10)
+  end
+
+  describe 'place robot' do
+     it 'places robot in top left corner when given co-ords 0x0' do
+       warehouse.place_robot(0,0)
+       expect(warehouse.grid[0][0]).to eq(1)
+     end
+
+     it 'places robot in bottom right corner when coven coords 9x9' do
+       warehouse.place_robot(9,9)
+       expect(warehouse.grid[9][9]).to eq(1)
+     end
+  end
+
+  describe 'locate robot' do
+    it 'returns the current coordinates of the robot' do
+      warehouse.place_robot(5,3)
+      expect(warehouse.locate_robot).to eq([5, 3])
+    end
+  end
+
+  describe 'move robot' do
+    it 'moves the robot one square right when given direction E' do
+      warehouse.place_robot(9,0)
+      warehouse.move_robot('E')
+      expect(warehouse.grid[9][1]).to eq(1)
+      expect(warehouse.grid[9][0]).to eq(0)
+    end
+
+    it 'moves the robot one square left when given direction W' do
+      warehouse.place_robot(0,9)
+      warehouse.move_robot('W')
+      expect(warehouse.grid[0][9]).to eq(0)
+      expect(warehouse.grid[0][8]).to eq(1)
+    end
+
+    it 'moves the robot one square up when given direction N' do
+      warehouse.place_robot(1,9)
+      warehouse.move_robot('N')
+      expect(warehouse.grid[1][9]).to eq(0)
+      expect(warehouse.grid[0][9]).to eq(1)
+    end
   end
 
 end
